9c391040261a20f82866d63c7b4efdd9421e14d9
srdjan86 <srdjan.86@gmail.com>
2026-01-16 21:38:35 +0100

    Add marketData model and api

--- flutter_app/lib/models/market_data_model.dart
+++ flutter_app/lib/models/market_data_model.dart
@@ -1,22 +1,25 @@
-// TODO: Create MarketData model class
-// Required fields:
-// - symbol (String)
-// - price (double)
-// - change24h (double)
-// - changePercent24h (double)
-// - volume (double)
-//
-// Add a factory constructor fromJson that parses the JSON response
-// Example JSON structure from API:
-// {
-//   "symbol": "BTC/USD",
-//   "price": 43250.50,
-//   "change24h": 2.5,
-//   "changePercent24h": 2.5,
-//   "volume": 1250000000
-// }
-
 class MarketData {
-  // TODO: Add required fields and constructor
-  // TODO: Add factory MarketData.fromJson(Map<String, dynamic> json)
+  final String? symbol;
+  final double? price;
+  final double? change24h;
+  final double? changePercent24h;
+  final int? volume;
+
+  MarketData({
+    required this.symbol,
+    required this.price,
+    required this.change24h,
+    required this.changePercent24h,
+    required this.volume,
+  });
+
+  factory MarketData.fromJson(Map<String, dynamic> json) {
+    return MarketData(
+      symbol: json['symbol'],
+      price: json['price'],
+      change24h: json['change24h'],
+      changePercent24h: json['changePercent24h'],
+      volume: json['volume'],
+    );
+  }
 }
